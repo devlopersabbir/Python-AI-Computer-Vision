23c267926df1c9eda4be6afe50ce4639355878f3
Sabbir Hossain Shuvo <82939905+devlopersabbir@users.noreply.github.com>
2023-12-14 12:49:20 +0600

    [style - comments 🎗] each line comment in turtle draw

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ class Drawing:
     turt:Turtle = Turtle()
     screen = Screen()
     def left_right(self, isLeft:bool):
-        heading = self.turt.heading() + 20 if isLeft else self.turt.heading() - 20
+        heading = self.turt.heading() + 20 if isLeft else self.turt.heading() - 20 # left right condision
         self.turt.setheading(heading)
         self.turt.forward(20)
     def forward_backward(self, isFor:bool):
-        self.turt.forward(10) if isFor else self.turt.backward(10)
+        self.turt.forward(10) if isFor else self.turt.backward(10) # forward backward condision
     def clearScreen(self):
         self.turt.clear()  # Clears turtle drawings
         self.turt.penup()
-        self.turt.home()  # Move turtle to its home position (starting position)
+        self.turt.home()  # Move the turtle to its home position (starting position)
         self.turt.pendown()
     def run(self):
         self.screen.listen()
